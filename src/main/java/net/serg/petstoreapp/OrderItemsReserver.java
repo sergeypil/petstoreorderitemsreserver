63afea4c729726a34fed4dc9ca05c7772af8499c
Sergey Pil <realsergeypil@gmail.com>
2024-03-25 16:28:20 +0500

    Replace with ServiceBusQueueTrigger

--- src/main/java/net/serg/petstoreapp/OrderItemsReserver.java
+++ src/main/java/net/serg/petstoreapp/OrderItemsReserver.java
@@ -3,6 +3,8 @@ package net.serg.petstoreapp;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -11,29 +13,27 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public class OrderItemsReserver {
-    private static final String connectionString = "<Add when need>";
+    private static final String connectionString = "***";
     private static final String blobContainerName = "order-items-reserver";
 
     @FunctionName("reserveOrderItems")
-    public HttpResponseMessage run(
-        @HttpTrigger(
-                name = "req", methods = {HttpMethod.POST}, 
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<SessionData>> request,
+    public void run(
+        @ServiceBusQueueTrigger(
+            name = "sessionData",
+            queueName = "PetStoreQueue",
+            connection = "SERVICE_BUS_CONNECTION_STRING") //env variable
+        SessionData sessionData,
         final ExecutionContext context) {
 
         context.getLogger().info("reserveOrderItems Function triggered.");
-        context.getLogger().info("Request body: " + request.getBody().orElse(null));
-        SessionData sessionData = request.getBody().orElse(null);
-        if (sessionData == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Missing sessionData data.").build();
-        }
+        context.getLogger().info("Received sessionData: " + sessionData);
 
         try {
             String orderJson = sessionData.getOrderJson();
@@ -45,15 +45,14 @@ public class OrderItemsReserver {
             if (blobClient.exists()) {
                 blobClient.delete();  // Delete blob if it already exists
             }
-
-            // Create new blob and upload JSON data.
+            
+            // Create new blob and upload JSON data
             BlobClient newBlobClient = blobContainerClient.getBlobClient(sessionData.getSessionId() + ".json");
             newBlobClient.upload(new ByteArrayInputStream(orderJson.getBytes(StandardCharsets.UTF_8)), orderJson.length());
-            return request.createResponseBuilder(HttpStatus.OK).body("Order item reservation completed successfully.").build();
+            context.getLogger().info("Order item reservation completed successfully.");
 
         } catch (Exception e) {
             context.getLogger().severe("Error processing request: " + e.getMessage());
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while reserving sessionData items.").build();
         }
     }
 }
